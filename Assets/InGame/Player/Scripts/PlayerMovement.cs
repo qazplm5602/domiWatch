11cdd959c6d9a191e216b8ffd232614b690647e9
domi <t01048187045@gmail.com>
2023-05-18 00:53:00 +0900

    ㅑㅓㅏㄹ뉴너ㅐㅐㅓ댜ㅐㅗㄷㄱ

---
+++ Assets/InGame/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;      // 캐릭터 움직임 스피드.
+    public float jumpSpeed; // 캐릭터 점프 힘.
+    public float gravity;    // 캐릭터에게 작용하는 중력.
+    public float mouseSensitivity = 3f;
+ 
+    private CharacterController controller; // 현재 캐릭터가 가지고있는 캐릭터 컨트롤러 콜라이더.
+    private Vector3 MoveDir;                // 캐릭터의 움직이는 방향.
+    private float verticalRotation = 0f;
+    
+    void Start()
+    {
+        speed     = 6.0f;
+        jumpSpeed = 8.0f;
+        gravity   = 20.0f;
+ 
+        MoveDir = Vector3.zero;
+        controller = GetComponent<CharacterController>();
+    }
+ 
+    void Update()
+    {
+        // 현재 캐릭터가 땅에 있는가?
+        if (controller.isGrounded)
+        {
+            // 위, 아래 움직임 셋팅. 
+            MoveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+ 
+            // 벡터를 로컬 좌표계 기준에서 월드 좌표계 기준으로 변환한다.
+            MoveDir = transform.TransformDirection(MoveDir);
+ 
+            // 스피드 증가.
+            MoveDir *= speed;
+ 
+            // 캐릭터 점프
+            if (Input.GetButton("Jump"))
+                MoveDir.y = jumpSpeed;
+ 
+        }
+ 
+        // 캐릭터에 중력 적용.
+        MoveDir.y -= gravity * Time.deltaTime;
+ 
+        // 캐릭터 움직임.
+        controller.Move(MoveDir * Time.deltaTime);
+
+        // Get mouse movement inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        // Rotate the camera vertically
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+
+        // Rotate the character horizontally
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
