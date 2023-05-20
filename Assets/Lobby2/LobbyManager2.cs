84d595f3106be2ea17dfa3a93d978029fb9e6126
domi <domi@domi.kr>
2023-05-20 18:17:46 +0900

    이름도 이제 수정 됨 ㅅㄱ

--- Assets/Lobby2/LobbyManager2.cs
+++ Assets/Lobby2/LobbyManager2.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LobbyManager2 : MonoBehaviour
 {
     public static string MyName;
     public static string MyID;
 
+    [SerializeField] TextMeshProUGUI NameText;
+
     private void Start() {
+        NameText.text = MyName;
         print(MyName);
         print(MyID);
     }
